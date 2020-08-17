58264a05f53a3f77b30cb0b43f1a48c18af72630
Andrew <trumanpvt@gmail.com>
2020-08-18 02:01:14 +0300

    search, some improvements

--- src/components/CardsPage/index.jsx
+++ src/components/CardsPage/index.jsx
@@ -1,20 +1,71 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useInput } from 'react';
+import axios from 'axios';
+
+// import Search from '../search';
+import Card from '../card';
+
 import './index.css';
 
 function CardsPage() {
   const [cardsData, setCardsData] = useState([]);
+  const [cardsSearch, setCardsSearch] = useState('');
+
   useEffect(() => {
-    const url = 'https://api.github.com/repos/trumanpvt/iteco/json';
-    fetch(url).then(
-      (res) => setCardsData(res),
+    const fetchData = async () => {
+      const result = await axios(
+        'https://api.jsonbin.io/b/5f3ae7b24d9399103616cd5e',
+      );
+
+      setCardsData(result.data.cards);
+    };
+
+    fetchData();
+  }, []);
+
+  function renderSearch(cards) {
+    console.log(cards);
+
+    return (
+      <div>
+        <form autoComplete="off">
+          <label htmlFor="search">
+            Поиск карт:
+            <input
+              name="search"
+              type="text"
+              value={cardsSearch}
+              onChange={(e) => setCardsSearch(e.target.value.replace(/\D/, ''))}
+            />
+          </label>
+        </form>
+      </div>
     );
-  });
+  }
+
+  function renderCard(card) {
+    const cardNumber = card.cardNumber.slice(card.cardNumber.length - 4);
+
+    if (!cardsSearch || cardNumber.includes(cardsSearch)) {
+      return (
+        <Card
+          key={card.cardNumber}
+          card={card}
+          cardNumber={cardNumber}
+          className="card"
+        />
+      );
+    }
+
+    return null;
+  }
 
   return (
     <>
-      <div>Test div</div>
+      <div>{renderSearch(cardsData)}</div>
       <div>
-        {cardsData}
+        {cardsData.map((card) => (
+          renderCard(card)
+        ))}
       </div>
     </>
   );
--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,14 +1,71 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
 import './index.css';
 
-function Card() {
-  const [count, setCount] = useState(0);
+function Card(props) {
+  const [expandCard, setExpandCard] = useState(false);
+  const { card, cardNumber } = props;
+  const date = new Date(card.expiringDate).toLocaleDateString();
+  const currency = card.currency === 'RUR' ? '₽' : '$';
 
   return (
     <>
-
+      <div className="card">
+        <div className="cardHeader">
+          <div className="cardHeader__top">
+            <div className="cardTitle">
+              {`${card.title}, ${currency}`}
+            </div>
+            <div className="cardDate">
+              {date}
+            </div>
+            <button
+              role="button"
+              className="cardHeader__buttonExpand"
+              onClick={() => setExpandCard(!expandCard)}
+            >
+              <div
+                className={`cardHeader__buttonIcon ${expandCard ? 'cardHeader__buttonIcon_rotate' : ''}`}
+              />
+            </button>
+          </div>
+          <div className="cardHeader__bottom">
+            {`***${cardNumber}`}
+          </div>
+        </div>
+        <div
+          className={`cardDescription ${expandCard ? 'cardDescription_show' : 'cardDescription_hide'}`}
+        >
+          {card.description}
+        </div>
+      </div>
     </>
   );
 }
+
+Card.propTypes = {
+  card: PropTypes.shape({
+    // cardNumber: PropTypes.string,
+    currency: PropTypes.string,
+    description: PropTypes.string,
+    expiringDate: PropTypes.string,
+    title: PropTypes.string,
+    value: PropTypes.number,
+  }),
+  cardNumber: PropTypes.string,
+};
+
+Card.defaultProps = {
+  card: {
+    // cardNumber: '',
+    currency: '',
+    description: '',
+    expiringDate: '',
+    title: '',
+    value: '',
+  },
+  cardNumber: '',
+};
 
 export default Card;
